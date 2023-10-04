a29f3c366530794510411cb8314cdb643047c107
amikom <rifkitrwjya13@student.amikom.ac.id>
2023-10-04 17:02:15 +0700

    update praktikum5

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,12 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //instance
+        val btnlogin:Button = findViewById(R.id.buttonLogin)
+        val txtUsername:EditText = findViewById(R.id.editTextUsername)
+        val txtPassword:EditText = findViewById(R.id.editTextPassword)
+
+        //event saat button Login click
+        btnlogin.setOnClickListener(
+            //login cek
+        if(txtUsername.text.toString().equals("rifki")&&
+                txtPassword.text.toString().equals("amikom")) {
+            //call manager
+            val intent =Intent( this,MainActivity::class.java)
+            startActivity(intent)
+        }else {
+            Toast.makeText(this,"Login gagal,cek username dan password", Toast.LENGTH_SHORT).show()
+        }
+        )
     }
 }
